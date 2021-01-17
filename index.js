f45b78690f5e3d67120bc68dad88f3eda7d21b5d
Paul Vatterott <71739337+pfvatterott@users.noreply.github.com>
2021-01-16 17:01:51 -0700

    Added index.js and transfered over all JS from HTML

---
+++ index.js
@@ -0,0 +1,115 @@
+// Local Store Pull
+var cityList = JSON.parse(localStorage.getItem("allCities"));
+if (cityList != null) {
+    generateCityList(cityList);
+}
+
+
+// List of Cities Generator
+function generateCityList(cityList) {
+    $(".city-list-group").empty();
+    for (let i = 0; i < cityList.length; i++) {
+        var cityButtonList = $("<li>");
+        cityButtonList.addClass("list-group-item city-card").text(cityList[i].city);
+        cityButtonList.attr("type", "button");
+        $(".city-list-group").append(cityButtonList);
+    }
+}
+
+// Search City Event Listener
+$(".search-city-button").on("click", function () {
+    cityList = JSON.parse(localStorage.getItem("allCities"));
+    if (cityList != null) {
+        cityList.push({ city: $(".search-city-form").val() });
+        localStorage.setItem("allCities", JSON.stringify(cityList));
+        generateCityList(cityList);
+        getCityCoords($(".search-city-form").val());
+    }
+    else {
+        cityList = [{ city: $(".search-city-form").val() }];
+        localStorage.setItem("allCities", JSON.stringify(cityList));
+        generateCityList(cityList);
+        getCityCoords($(".search-city-form").val());
+    }
+})
+
+// City List Event Listener
+$(document).on("click", ".city-card", function () {
+    console.log($(this).text())
+    getCityCoords($(this).text());
+})
+
+function getCityCoords(cityName) {
+    $(".city-name").text(cityName)
+    var coordsURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=362001ca18aa50ad6cce0cd209741e8b";
+    $.ajax({
+        url: coordsURL,
+        method: "GET",
+        success: function (coordsResponse) {
+            console.log(coordsResponse);
+            var lat = coordsResponse.coord.lat;
+            var lon = coordsResponse.coord.lon;
+            getCityWeather(lat, lon);
+        }
+    })
+}
+
+
+function getCityWeather(lat, lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=362001ca18aa50ad6cce0cd209741e8b";
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        success: function (response) {
+            console.log(response)
+            //current weather
+            var currentIcon = "https://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png";
+            var currentTemp = response.current.temp;
+            var currentHumidity = response.current.humidity;
+            var currentWind = response.current.wind_speed;
+            var currentUV = response.current.uvi;
+            var currentPrecip = response.daily[0].pop * 100;
+            var currentDescription = response.current.weather[0].description;
+            $(".city-icon").attr("src", currentIcon);
+            $(".city-description").text(currentDescription);
+            $(".city-temp").text("Temperature: " + currentTemp + " °F");
+            $(".city-precip").text("Precipitation: " + currentPrecip + "%")
+            $(".city-humidity").text("Humidity: " + currentHumidity + "%");
+            $(".city-wind").text("Wind Speed: " + currentWind + " MPH")
+            $(".city-UV").text("UV-Index: " + currentUV);
+            //5 day forecast
+            for (let i = 1; i < response.daily.length - 2; i++) {
+                var forecastIcon = "https://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png";
+                var forecastTemp = response.daily[i].temp.day;
+                var forecastPrecip = response.daily[i].pop * 100;
+                var forecastHumidity = response.daily[i].humidity;
+                var forecastDay = moment.unix(response.daily[i].dt).format('ddd D');
+                $(".city-day").each(function () {
+                    if (i == $(this).attr("data-value")) {
+                        $(this).empty();
+                        $(this).append(forecastDay);
+                    }
+                })
+                $(".city-card-daily").each(function () {
+                    if (i == $(this).attr("data-value")) {
+                        $(this).empty();
+                        $(this).append("Temp: " + forecastTemp + " °F" + "<br>" + "Humidity: " + forecastHumidity 
+                        + "%" + "<br>" + "Precipitation: " + forecastPrecip + "%");
+                    }
+                })
+                $(".city-card-icon").each(function() {
+                    if (i == $(this).attr("alt")) {
+                        $(this).attr("src", forecastIcon);
+                    }
+                })
+            }
+        }
+    })
+}
+
+// If no local available run default New York City
+if (cityList === null) {
+    getCityCoords("New York City");
+} else {
+    getCityCoords(cityList[0].city);
+}
